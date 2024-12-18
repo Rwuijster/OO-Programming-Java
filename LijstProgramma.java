98dc16498fc84278ea63fdd49b1cfc6f720b8030
Rwuijster <60777294+Rwuijster@users.noreply.github.com>
2024-12-18 11:13:37 +0100

    LijstProgramma.java

--- LijstProgramma.java
+++ LijstProgramma.java
@@ -4,7 +4,20 @@ import verkiezingen.Partij;
 import verkiezingen.Kandidaat;
 
 public class LijstProgramma {
-  public static void main(String[] args) {	        
+  public static void main(String[] args) {
+    Stemmachine machine = new Stemmachine();
+    machine.zetAan();
+    
+    ArrayList<Partij> partijlijst = machine.getPartijen();
+    Partij laatstePartij = partijlijst.get(2);
+    ArrayList<Kandidaat> kandidaatLijst = laatstePartij.getKandidaten();
+    Kandidaat laatsteKandidaat = kandidaatLijst.get(4);
+    
+    System.out.println("De laatste Partij is: " + laatstePartij.getNaam());
+    System.out.println("De laatste kandidaat is: " + laatsteKandidaat.getNaam());
+    
+    
+    
 
   }
 }
