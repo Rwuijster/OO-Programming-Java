c56a723e70a51694acb887508185ab5078311b05
Rwuijster <60777294+Rwuijster@users.noreply.github.com>
2024-12-18 11:15:16 +0100

    Update Lijstprogramma.java

--- LE03/Lijstprogramma.java
+++ LE03/Lijstprogramma.java
@@ -1,1 +1,23 @@
+import java.util.ArrayList;
+import verkiezingen.Stemmachine;
+import verkiezingen.Partij;
+import verkiezingen.Kandidaat;
 
+public class LijstProgramma {
+  public static void main(String[] args) {
+    Stemmachine machine = new Stemmachine();
+    machine.zetAan();
+    
+    ArrayList<Partij> partijlijst = machine.getPartijen();
+    Partij laatstePartij = partijlijst.get(2);
+    ArrayList<Kandidaat> kandidaatLijst = laatstePartij.getKandidaten();
+    Kandidaat laatsteKandidaat = kandidaatLijst.get(4);
+    
+    System.out.println("De laatste Partij is: " + laatstePartij.getNaam());
+    System.out.println("De laatste kandidaat is: " + laatsteKandidaat.getNaam());
+    
+    
+    
+
+  }
+}
